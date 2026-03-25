1c222ca6ffcd9e7be040295401299d778e6990fc
Hokutolite <hokutolite217@gmail.com>
2026-03-25 21:28:38 +0800

    Add WinMain function to display message box

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <windows.h>
+int WINAPI WinMain(HINSTANCE h, HINSTANCE p, LPSTR c, int s) {
+    MessageBox(NULL, "這是從 GitHub 雲端產生的原生程式！", "專業工具", MB_OK | MB_ICONINFORMATION);
+    return 0;
+}
